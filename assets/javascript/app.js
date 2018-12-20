6f8640138771c8389ba7e0d8f21a26d4899e1f0b
joura <martinezluisjorge86@gmail.com>
2018-12-20 15:34:33 -0500

    images, ques/ans array, minimal html

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+// Let the page load first{
+
+var trivia = [
+    {
+        question0: "What is Spider-Man's real identity?",
+        choice0: ['A. Peter Porker', 'B. Peter Parker', 'C. Reed Richards', 'D. Peter Griffin'],
+        answer0: [1],
+    },
+    {
+        question1: "Who takes care of Spider-Man while he's in High School?",
+        choice1: ['A. Professor Xavier', 'B. Doctor Conners', 'C. Sue and Reed Richards', 'D. Aunt May'],
+        answer1: [3],
+
+    },
+    {
+        question2: "Who calls Spider-Man, 'Tiger'?",
+        choice2: ['A. Mary-Jane Watson', 'B. Pepper Potts', 'C. Gwen Stacey', 'D. Felicia Hardy'],
+        answer2: [0],
+    },
+    {
+        question3: "What is the name of the first symbiote introduced?",
+        choice3: ['A. Carnage', 'B. Venom', 'C. Poison' , 'D. Vengeance'],
+        answer3: [1],
+    },
+
+    {
+        question4: "Where does Spider-Man typically web-sling?",
+        choice4: ['A. Los Angeles', 'B. Chicago', 'C. New York City' , 'D. Miami'],
+        answer4: [2],
+    },
+    {
+        question5: "Which Fantastic Four member does Spider-Man temporarily replace?",
+        choice5: ['A. Mr. Fantastic', "B. Invisible Woman" , 'C. The Thing' , 'D. The Human Torch'],
+        answer5: [3],
+    },
+    {
+        question6: "The Original Sinister Six is made up of: Doctor Octopus, Kraven the Hunter, Electro, Mysterio, Sand-Man, and ?",
+        choice6: ['A. Vulture' , 'B. Kingpin' , 'C. Scorpion' , 'D. Rhino'],
+        answer6: [0]
+
+    }
+]
+
+
+
+
+
+
+console.log(trivia);
+
+$(document).ready(function () {
+
+});
+// console.log(spiderQuestions);
